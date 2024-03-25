7e2be449eaca58829148af6030522b23315b6b48
DGFR <100404424+danangfir@users.noreply.github.com>
2024-03-25 22:45:50 +0700

    selesai juga

--- scraper.js
+++ scraper.js
@@ -7,20 +7,21 @@ const { chromium } = require('playwright');
     await page.goto('https://bachacoffee.com/en/coffee-beans', { waitUntil: 'domcontentloaded' });
 
     await page.waitForSelector('.product-listing__coffee--body', { state: 'attached' });
+
+    await autoScroll(page);
+
+    await page.evaluate(() => window.scrollBy(0, window.innerHeight));
+    await page.waitForTimeout(17000); 
     
-    await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-    await page.waitForTimeout(1000); // Beri waktu untuk konten dimuat setelah scroll
-
     const outerElements = await page.$$('.product-listing__coffee--body');
-
     console.log(`Found ${outerElements.length} outer elements.`);
 
     for (const outerElement of outerElements) {
       const innerElement1 = await outerElement.$('.product-listing__coffee--name');
       const innerElement2 = await outerElement.$('.price--block__number');
 
-      const text1 = await innerElement1.textContent();
-      const text2 = await innerElement2.textContent();
+      const text1 = innerElement1 ? await innerElement1.textContent() : 'Nama produk tidak ditemukan';
+      const text2 = innerElement2 ? await innerElement2.textContent() : 'Harga produk tidak ditemukan';
 
       console.log({ text1: text1.trim(), text2: text2.trim() });
     }
@@ -31,3 +32,22 @@ const { chromium } = require('playwright');
   }
 })();
 
+
+async function autoScroll(page){
+  await page.evaluate(async () => {
+      await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100; 
+          var timer = setInterval(() => {
+              var scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+
+              if(totalHeight >= scrollHeight){
+                  clearInterval(timer);
+                  resolve();
+              }
+          }, 100); 
+      });
+  });
+}
